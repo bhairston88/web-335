e550ceade14040b48da4b212336a6608b42ae8a0
Brooklyn Hairston <bhairston@my365.bellevue.edu>
2020-12-02 15:57:31 -0500

    adding assignment 8.2

---
+++ week-8/hairston_hello_world.py
@@ -0,0 +1,11 @@
+# 
+# ============================================
+# Title: hairston_hello_world.py
+# Author: Professor Massoud
+# Date:   2 December 2020
+# Modified By: Brooklyn Hairston
+# Description: Demonstrates how to use 
+# ===========================================
+#
+
+print('Hello World')
